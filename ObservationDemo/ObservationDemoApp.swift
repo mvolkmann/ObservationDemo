d6071aea6027d4a24ebc5566b2ea86e3950e8fcf
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-21 12:50:47 -0500

    Initial Commit

---
+++ ObservationDemo/ObservationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationDemoApp.swift
+//  ObservationDemo
+//
+//  Created by Mark Volkmann on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ObservationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
