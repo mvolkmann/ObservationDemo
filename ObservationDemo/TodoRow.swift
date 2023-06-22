e6d3c905c379cf129d28c74419e8efa6e23c0fab
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-22 09:52:11 -0500

    progress

--- ObservationDemo/TodoRow.swift
+++ ObservationDemo/TodoRow.swift
@@ -7,30 +7,9 @@ struct TodoRow: View {
 
     var body: some View {
         HStack {
-            // TODO: Is there a way to bind to the done property
-            // TODO: of a specific todo?
-            // Toggle("", isOn: todo.done)
-
-            /*
-             Button {
-                 vm.toggleTodo(todo)
-             } label: {
-                 let name = todo
-                     .done ? "checkmark.square" : "square"
-                 Image(systemName: name)
-             }
-             // Without this, tapping either button triggers both!
-             .buttonStyle(.borderless)
-             */
-
             Checkbox(label: todo.description, isOn: $todo.done)
-
-            Text(todo.description)
-                .foregroundStyle(todo.done ? .gray : .black)
                 .strikethrough(todo.done)
-
             Spacer()
-
             Button {
                 vm.deleteTodo(todo)
             } label: {
